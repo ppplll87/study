3bf48a873d8a41d6922acf9e8d64d0810b9f8550
ppplll87 <egpower3@gmail.com>
2017-05-15 18:44:56 +0900

    test commit

--- src/test/CommonTest.java
+++ src/test/CommonTest.java
@@ -2,11 +2,14 @@ package test;
 
 import org.junit.Test;
 
+import utils.MathUtil;
+
 public class CommonTest {
 
 	@Test
 	public void test() {
-		System.out.println("hi");
+		System.out.println(MathUtil.getPermutation(10, 2));
+		System.out.println(MathUtil.getCombination(10, 2));
 	}
 
 }
